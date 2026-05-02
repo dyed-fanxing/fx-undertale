9c7288fc74c1a95a80c5f5352c40573d90f088e2
dyed_fanxing <1941501914@qq.com>
2026-05-02 13:36:44 +0800

    GB炮光束渲染阶段提取到实体渲染final之后

--- src/main/java/com/fanxing/fx_undertale/client/render/entity/summon/GasterBlasterRender.java
+++ src/main/java/com/fanxing/fx_undertale/client/render/entity/summon/GasterBlasterRender.java
@@ -47,7 +47,13 @@ public class GasterBlasterRender extends GeoEntityRenderer<GasterBlaster> {
     @Override
     public void postRender(PoseStack poseStack, GasterBlaster animatable, BakedGeoModel model, @Nullable MultiBufferSource bufferSource, @Nullable VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, int colour) {
         super.postRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, colour);
-        if (!isReRender && !animatable.isMountable()) {
+
+    }
+
+    @Override
+    public void renderFinal(PoseStack poseStack, GasterBlaster animatable, BakedGeoModel model, MultiBufferSource bufferSource, @Nullable VertexConsumer buffer, float partialTick, int packedLight, int packedOverlay, int colour) {
+        super.renderFinal(poseStack, animatable, model, bufferSource, buffer, partialTick, packedLight, packedOverlay, colour);
+        if (!animatable.isMountable()) {
             GasterBlasterBeamRenderer.render(animatable,poseStack, bufferSource, partialTick,  animatable.colors);
         }
     }
